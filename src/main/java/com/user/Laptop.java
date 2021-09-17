75a90d888cbc7cb4c2af5f42b55472352df2583f
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-09-18 02:39:03 +0530

    Connection established

--- src/main/java/com/user/Laptop.java
+++ src/main/java/com/user/Laptop.java
@@ -1,9 +1,53 @@
 package com.user;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "laptop")
 public class Laptop {
+    @Id
+    @Column(name = "lid")
     private int lid ;
+    @Column(name = "lname")
     private String lname ;
-    private String
+    @Column(name = "price")
+    private double price ;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "lid=" + lid +
+                ", lname='" + lname + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
 
 
