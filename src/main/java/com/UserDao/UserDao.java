9896c034daf188729b90d775e2ccd7cd867de0a2
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-09-18 02:39:03 +0530

    Connection established

--- src/main/java/com/UserDao/UserDao.java
+++ src/main/java/com/UserDao/UserDao.java
@@ -1,10 +1,29 @@
 package com.UserDao;
 
+import com.user.Laptop;
 import com.user.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class UserDao {
     public static void main(String[] args) {
-        User u = new User();
+        Laptop lapy = new Laptop();
+        lapy.setLid(101);
+        lapy.setLname("Dell");
+        lapy.setPrice(40000);
+
+        Configuration con = new Configuration().configure().addAnnotatedClass(Laptop.class);
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        session.save(lapy);
+
+        tx.commit();
 
     }
 }
